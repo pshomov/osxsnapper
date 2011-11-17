c54086fb4a17cc586b79245b2bd94664b9969946
Petar Shomov <pshomov@gmail.com>
2011-11-17 10:49:41 +0000

    better formting of the file names of the images

--- snapper.rb
+++ snapper.rb
@@ -13,6 +13,6 @@ while(true)
   sleep(15.0)
   idletime = CGEventSourceSecondsSinceLastEventType(1,NX_KEYDOWN | NX_LMOUSEDOWN | NX_LMOUSEUP | NX_RMOUSEDOWN | NX_RMOUSEUP | NX_MOUSEMOVED | NX_LMOUSEDRAGGED | NX_RMOUSEDRAGGED | NX_KEYDOWN | NX_KEYUP | NX_FLAGSCHANGED | NX_SCROLLWHEELMOVED)
   if (idletime < 60)  
-    Screen.capture.save("#{Time.new.strftime("%Y%m%dT%H%M%S")} - desktop.jpg") 
+    Screen.capture.save("#{Time.new.strftime("%Y%m%d - %H:%M:%S")} - desktop.jpg") 
   end
 end
