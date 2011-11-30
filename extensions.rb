d2a229e0524bc545085cc03c9eefe693b3c53d71
Petar Shomov <pshomov@gmail.com>
2011-11-30 09:03:58 +0000

    fine tuning the generation of the screenshots

--- extensions.rb
+++ extensions.rb
@@ -1,7 +1,7 @@
   class CIImage      
     def save(target, format = NSJPEGFileType, properties = nil)
       bitmapRep = NSBitmapImageRep.alloc.initWithCIImage(self)
-      blob = bitmapRep.representationUsingType(format, properties:properties)
+      blob = bitmapRep.representationUsingType(format, properties:{NSImageProgressive => true, NSImageCompressionFactor => 0.3})
       blob.writeToFile(target, atomically:true)
     end
   end
