396e954e0b0f06b7e071e1eee48a5a21a6bb3e08
Petar Shomov <pshomov@gmail.com>
2011-11-30 08:23:16 +0000

    dont's stop recording when there is an exception as well as a better memory management

--- extensions.rb
+++ extensions.rb
@@ -2,15 +2,6 @@
     def save(target, format = NSJPEGFileType, properties = nil)
       bitmapRep = NSBitmapImageRep.alloc.initWithCIImage(self)
       blob = bitmapRep.representationUsingType(format, properties:properties)
-      blob.writeToFile(target, atomically:false)
-    end
-
-    def cgimage
-      OSX::NSBitmapImageRep.alloc.initWithCIImage(self).CGImage()
-    end
-
-    def self.from(filepath)
-      raise Errno::ENOENT, "No such file or directory - #{filepath}" unless File.exists?(filepath)
-      OSX::CIImage.imageWithContentsOfURL(OSX::NSURL.fileURLWithPath(filepath))
+      blob.writeToFile(target, atomically:true)
     end
   end
--- snapper.rb
+++ snapper.rb
@@ -5,14 +5,22 @@ require 'time'
 class Screen
   def self.capture
     screenshot = CGWindowListCreateImage(CGRectInfinite, KCGWindowListOptionOnScreenOnly, KCGNullWindowID, KCGWindowImageDefault)
-    CIImage.imageWithCGImage(screenshot)
+    image = CIImage.imageWithCGImage(screenshot)
+    CGImageRelease screenshot
+    image
   end
 end
 
 while(true)
   sleep(15.0)
-  idletime = CGEventSourceSecondsSinceLastEventType(1,NX_KEYDOWN | NX_LMOUSEDOWN | NX_LMOUSEUP | NX_RMOUSEDOWN | NX_RMOUSEUP | NX_MOUSEMOVED | NX_LMOUSEDRAGGED | NX_RMOUSEDRAGGED | NX_KEYDOWN | NX_KEYUP | NX_FLAGSCHANGED | NX_SCROLLWHEELMOVED)
-  if (idletime < 60)  
-    Screen.capture.save("#{Time.new.strftime("%Y%m%d - %H:%M:%S")} - desktop.jpg") 
+  begin
+    idletime = CGEventSourceSecondsSinceLastEventType(1,NX_KEYDOWN | NX_LMOUSEDOWN | NX_LMOUSEUP | NX_RMOUSEDOWN | NX_RMOUSEUP | NX_MOUSEMOVED | NX_LMOUSEDRAGGED | NX_RMOUSEDRAGGED | NX_KEYDOWN | NX_KEYUP | NX_FLAGSCHANGED | NX_SCROLLWHEELMOVED)
+    if (idletime < 60)  
+      image = Screen.capture
+      Dir::mkdir "#{Time.new.strftime("%Y%m%d")}" unless Dir::exists? "#{Time.new.strftime("%Y%m%d")}"
+      image.save("#{Time.new.strftime("%Y%m%d")}/#{Time.new.strftime("%Y%m%d - %H:%M:%S")} - desktop.jpg") 
+    end
+  rescue Exception => e
+    puts e.message
   end
 end
